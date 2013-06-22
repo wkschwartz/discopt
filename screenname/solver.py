0f0f37bb9d76964c2e5e0e08248c98df5c6a5c74
William Schwartz <wkschwartz@gmail.com>
2013-06-22 14:25:55 -0400

    Modernize solver.py

---
+++ screenname/solver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+
+def solveIt():
+	"Return a string that is the screen name you would like."
+	return 'I_R a-N00B'
+
+if __name__ == '__main__':
+	print('This script submits the screen name:', solveIt())
