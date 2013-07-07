8b884483fbbb317095996e1210e720048e05efb4
William Schwartz <wkschwartz@gmail.com>
2013-07-07 15:57:28 -0400

    Update test objective values

--- coloring/test.py
+++ coloring/test.py
@@ -10,7 +10,7 @@ class TestColoring(ta.TestAnswers):
 
 	PREFIX = 'gc'
 	PROBLEMS = ((50, 3), (70, 7), (100, 5), (250, 9), (500, 1), (1000, 5))
-	EXPECTED_ANSWERS = (6, 17, 15, 74, 13, 95)
+	EXPECTED_ANSWERS = (6, 17, 15, 73, 13, 85) # The latter two are wrong
 
 
 load_tests = ta.load_tests_factory(TestColoring)
