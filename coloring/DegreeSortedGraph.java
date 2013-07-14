8d0f5e5fb5c2c524b14dd9d169760b3056ba41d7
William Schwartz <wkschwartz@gmail.com>
2013-07-14 10:20:33 -0400

    Add Javadoc header to DegreeSortedGraph

--- coloring/DegreeSortedGraph.java
+++ coloring/DegreeSortedGraph.java
@@ -2,6 +2,17 @@ import edu.princeton.cs.algs4.Graph;
 import java.util.Comparator;
 import java.util.Arrays;
 
+
+/**
+ * A graph with the nodes relabeled so the higher-degree nodes come first.
+ * <p>
+ * Let <code>g</code> be a <code>Graph</code> instance. Then
+ * <code>dsg.reverse</code> such that <code>dsg = DegreeSortedGraph(g)</code> is
+ * an isomorphism that maps nodes of <code>dsg</code> to nodes of
+ * <code>g</code>. Further, if <var>i</var> < <var>j</var> then the degree of
+ * node <var>i</var> of <code>dsg</code> is at least the degree of node
+ * <var>j</var> or <code>dsg</code>.
+ */
 public class DegreeSortedGraph extends Graph {
 
 	private final int[] reverse;
